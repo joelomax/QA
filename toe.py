a3a9d6367d6da0b2f4ec0e5fbe08d576d819bea3
joelomax <joseph.r.lomax@gmail.com>
2020-02-18 23:46:39 +0000

    toe.py 2nd commit

--- toe.py
+++ toe.py
@@ -2,7 +2,12 @@
 TOE: Write a program that will find all such numbers which are divisible by 7, but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be returned in a comma-separated sequence on a single line.
 This must be on your GitHub by 9am tomorrow morning, and 2 random people will be chosen to pull their GitHub up and talk through their solution.
 """
-max = 3200
-for i in range (2000, max):
+flag = 0
+for i in range (2000, 3200):
     if i % 7 == 0 and i % 5 != 0:
-        print(i, end =",")
+        if flag ==0:
+            print(i, end="")
+            flag = 1
+        else:
+            print (",", end = "")
+            print(i, end ="")
